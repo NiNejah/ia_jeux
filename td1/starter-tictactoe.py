af1c430ebfc230715e0b8a449d7f99259c6ab0b6
NiNejah <almawlanejeh@gmail.com>
2022-11-15 14:46:00 +0100

    add : EXO2.2 first implementation

--- td1/starter-tictactoe.py
+++ td1/starter-tictactoe.py
@@ -5,11 +5,11 @@ import Tictactoe
 from random import randint,choice
 
 def RandomMove(b):
-    '''Renvoie un mouvement au hasard sur la liste des mouvements possibles'''
+    """Renvoie un mouvement au hasard sur la liste des mouvements possibles"""
     return choice(b.legal_moves())
 
 def deroulementRandom(b):
-    '''Effectue un déroulement aléatoire du jeu de morpion.'''
+    """Effectue un déroulement aléatoire du jeu de morpion."""
     print("----------")
     print(b)
     if b.is_game_over():
@@ -27,8 +27,8 @@ def deroulementRandom(b):
 
 
 def getresult(b):
-    '''Fonction qui évalue la victoire (ou non) en tant que X. Renvoie 1 pour victoire, 0 pour 
-       égalité et -1 pour défaite. '''
+    """Fonction qui évalue la victoire (ou non) en tant que X. Renvoie 1 pour victoire, 0 pour
+       égalité et -1 pour défaite. """
     if b.result() == b._X:
         return 1
     elif b.result() == b._O:
@@ -45,6 +45,7 @@ Combien votre arbre de recherche a-t-il de noeuds ? Combien de temps faut-il pou
 """
 def possibleGamesAux(b ,games,nodes):
     if b.is_game_over():
+        # eval(b)
         games.append(1)
     else:
         for m in b.legal_moves():
@@ -58,6 +59,22 @@ def possibleGames(b):
     possibleGamesAux(b,game,nodes)
     return len(game),len(nodes)
 
+def existWinningStrategyAux(b : Tictactoe.Board, winningGames = None):
+    if b.is_game_over() and getresult(b) == 1 :
+        if(winningGames == None):
+            return True
+        else:
+            winningGames.append(1)
+    else:
+        for m in b.legal_moves():
+            b.push(m)
+            if existWinningStrategyAux(b,winningGames):
+                return True
+            b.pop()
+        return False
+def existWinningStrategy(b : Tictactoe.Board ):
+    return existWinningStrategyAux(b)
+
 def main ():
     board = Tictactoe.Board()
     # print(board)
@@ -66,9 +83,12 @@ def main ():
     # print("Apres le match, chaque coup est défait (grâce aux pop()): on retrouve le plateau de départ :")
     # print(board)
     start = time.time()
-    g,n = possibleGames(board)
+    isThere = existWinningStrategy(board)
+    # g,n = possibleGames(board)
     end = time.time()
-    print(f"we have {g} games and {n} nodes \ntime of execution {(end -start)} s");
+    print("there is", " not " if not isThere else " ")
+    print(f"time : {(end -start)} s")
+    # print(f"we have {g} games and {n} nodes \ntime of execution {(end -start)} s");
 main()
 
 
--- td2/starter-chess.py
+++ td2/starter-chess.py
@@ -21,6 +21,53 @@ def deroulementRandom(b):
     deroulementRandom(b)
     b.pop()
 
-board = chess.Board()
-deroulementRandom(board)
+def possibleGamesAux(b ,depth,games,nodes):
+    if b.is_game_over() or depth == 0:
+        # eval(b)
+        games.append(1)
+    else:
+        for m in b.generate_legal_moves():
+            b.push(m)
+            nodes.append(1)
+            possibleGamesAux(b,depth -1 ,games,nodes)
+            b.pop()
+def possibleGames(b , depth = 3):
+    game=[]
+    nodes=[]
+    possibleGamesAux(b,depth,game,nodes)
+    return len(game),len(nodes)
 
+
+def evalue( board , turn = "AMI"):
+    value = {
+        'K':200,
+        'Q':9,
+        'R': 5,
+        'B': 3 ,
+        'N':3 ,
+        'P':1,
+        '.':0
+    }
+    scour , posfac = 0 , .2
+    for k,p in board.piece_map().items():
+        l = p.symbol()
+        u = l.upper()
+        pos = k // 8
+        # upper AMI ;
+        if(l==u): scour += value[l] + (pos * posfac)
+        # lower ENNEMI :
+        # TODO : fix - (pos * posfac)
+        else: scour -= value[u] - (pos * posfac)
+    return scour
+
+def main():
+    board = chess.Board()
+    # deroulementRandom(board)
+    for depth in range(1,10):
+        start = time.time()
+        g,n = possibleGames(board ,depth)
+        end = time.time()
+        print(f"for depth = {depth} we have {g} games and {n} nodes \ntime of execution {(end -start)} s");
+        print("--------------------------")
+
+main()
